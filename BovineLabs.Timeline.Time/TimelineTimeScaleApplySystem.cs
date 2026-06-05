37943998fd3a3a8a47153097776f43037449f206
IAFahim <iafahim.dev@gmail.com>
2026-06-05 19:50:56 +0600

    Removed stupid hit stop

--- BovineLabs.Timeline.Time/TimelineTimeScaleApplySystem.cs
+++ BovineLabs.Timeline.Time/TimelineTimeScaleApplySystem.cs
@@ -1,8 +1,6 @@
-using BovineLabs.HitStop.Data;
 using BovineLabs.Timeline.Data.Schedular;
 using BovineLabs.Timeline.Schedular;
 using Unity.Burst;
-using Unity.Collections;
 using Unity.Entities;
 
 namespace BovineLabs.Timeline.Time
@@ -10,53 +8,29 @@ namespace BovineLabs.Timeline.Time
     [UpdateInGroup(typeof(ScheduleSystemGroup))]
     [UpdateAfter(typeof(ClockUpdateSystem))]
     [UpdateBefore(typeof(TimerUpdateSystem))]
-    [Unity.Entities.WorldSystemFilter(Unity.Entities.WorldSystemFilterFlags.LocalSimulation | Unity.Entities.WorldSystemFilterFlags.ClientSimulation | Unity.Entities.WorldSystemFilterFlags.ServerSimulation)]
+    [WorldSystemFilter(WorldSystemFilterFlags.LocalSimulation | WorldSystemFilterFlags.ClientSimulation | WorldSystemFilterFlags.ServerSimulation)]
     public partial struct TimelineTimeScaleApplySystem : ISystem
     {
-        private ComponentLookup<HitStopState> hitStopsLookup;
-        private ComponentLookup<HitStopRemainingTime> remainingLookup;
-
         [BurstCompile]
         public void OnCreate(ref SystemState state)
         {
-            hitStopsLookup = state.GetComponentLookup<HitStopState>(true);
-            remainingLookup = state.GetComponentLookup<HitStopRemainingTime>(true);
         }
 
         [BurstCompile]
         public void OnUpdate(ref SystemState state)
         {
-            hitStopsLookup.Update(ref state);
-            remainingLookup.Update(ref state);
-
-            state.Dependency = new ApplyTimeScaleJob
-            {
-                HitStops = hitStopsLookup,
-                Remaining = remainingLookup
-            }.ScheduleParallel(state.Dependency);
+            state.Dependency = new ApplyTimeScaleJob().ScheduleParallel(state.Dependency);
         }
 
         [BurstCompile]
         private partial struct ApplyTimeScaleJob : IJobEntity
         {
-            [ReadOnly] public ComponentLookup<HitStopState> HitStops;
-            [ReadOnly] public ComponentLookup<HitStopRemainingTime> Remaining;
-
-            private void Execute(Entity entity, ref ClockData clock, in TimelineTimeScaleMultiplier multiplier)
+            private void Execute(ref ClockData clock, in TimelineTimeScaleMultiplier multiplier)
             {
-                var timeScale = multiplier.Value;
+                if (multiplier.Value == 1f) return;
 
-                if (HitStops.TryGetComponent(entity, out var hitStop) &&
-                    HitStops.IsComponentEnabled(entity) &&
-                    Remaining.TryGetComponent(entity, out var remaining) &&
-                    remaining.Value > 0f)
-                    timeScale = 0.0001f;
-
-                if (timeScale != 1f)
-                {
-                    clock.DeltaTime *= (double)timeScale;
-                    clock.Scale *= timeScale;
-                }
+                clock.DeltaTime *= (double)multiplier.Value;
+                clock.Scale *= multiplier.Value;
             }
         }
     }
